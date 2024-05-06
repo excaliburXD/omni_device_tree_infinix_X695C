21cfb196ff77b6afe90c48e5b4e9db3dd9105c7f
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2024-05-06 11:02:54 +0700

    adapt path clone

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-#git clone mhttps://github.com/transsion-mt6785/twrp-device_transsion_mt6785-common.git -b master device/transsion/mt6785-comon
+git clone https://github.com/transsion-mt6785/twrp-device_transsion_mt6785-common.git -b twrp device/transsion/mt6785-common
