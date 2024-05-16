fd1ca7c493d8a0248018c193388cb5b7f2ff35fc
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2024-05-16 16:27:09 +0700

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-#git clone https://github.com/transsion-mt6785/twrp-device_transsion_mt6785-common.git -b common device/transsion/mt6785-common
-git clone https://github.com/excaliburXD/twrp-device_transsion_mt6785-common.git -b common device/transsion/mt6785-common
